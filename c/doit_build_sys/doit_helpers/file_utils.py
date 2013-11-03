115c3dd97552730eab58195f850eb9d51ab2eaa3
Warwick Stone <uozu.aho@gmail.com>
2013-11-03 13:27:29 +1100

    Added dedicated doit helpers package

---
+++ c/doit_build_sys/doit_helpers/file_utils.py
@@ -0,0 +1,20 @@
+import fnmatch
+import os
+
+
+def find(path, pattern, exclude_pattern=None, recursive=True):
+    """ Return a list of files under the given path that match the
+        pattern. Searches all subdirectories by default.
+    """
+    matches = []
+    if recursive:
+        for root, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                if fnmatch(filename, pattern):
+                    matches.append(os.path.join(root, filename))
+    else:
+        for path in os.listdir(path):
+            if os.path.isfile(path) and fnmatch.fnmatch(path, pattern):
+                matches.append(os.path.join(path))
+
+    return [x for x in matches if not fnmatch.fnmatch(x, exclude_pattern)]
